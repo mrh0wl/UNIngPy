d9d1f6d7c7a63766c8d2e8c18c7bd568771c0b3e
MrH0wl <secmare>
2020-04-10 05:18:10 -0600

    Version <0.4.10> * SSL bug fix * ASCII decode fix * Adding of ExceptionHandler (beta)

--- UNIngPy/UNIngPy.py
+++ UNIngPy/UNIngPy.py
@@ -1,6 +1,8 @@
 
 from __future__ import print_function
 
+import os
+import sys
 import requests
 from bs4 import BeautifulSoup
 
@@ -26,18 +28,77 @@ class uniarticles(object):
 			redirect = ''.join(link[:1])
 			data = {'title': ases,
 					'date': date,
-					'preview': ps.text,
+					'preview': (ps.text).encode('ascii', 'ignore'),
 					'redirect': url+redirect}
 			res.append(data)
 		return res
 
+	def ExceptionHandler(self, message, response, exception, warn, function, *args, **kwargs):
+		difflib, exceptWarn, error, warnMessage, callback = [],[],[],[],[]
+		warns = []
+
+		try:
+			return function(*args, **kwargs)
+		except exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			x = str((exc_type, fname, exc_tb.tb_lineno)).split("'")[1]
+			count = len(x.split("."))
+		
+			for i in range(10):
+				for l in args:
+					try:
+						if l in str(e):
+							z = str(e).split(l+"'"+"\n")[i]
+						else:
+							z = str(e).split(":")[i]
+						if z not in warns:
+							warns.append(z.lstrip(' '))
+					except:
+						pass
+
+			try:
+				truefalse = "Default Python Library" if 'exceptions' in x.split('.')[0] else x.split('.')[0] 
+				difflib = "Default Python Library" if x.split('.')[0] is False and x.split('.')[1] is False else truefalse
+				exceptWarn = x.split('.')[count-2]
+				error = x.split('.')[count-1]
+				if "bool" in str(type(warn)):
+					raise TypeError
+				else:
+					warn = warn
+				if warn == 'AllWarnings':
+					warnMessage = warns
+				else:
+					warnMessage = warns[warn]
+			except TypeError:
+				t = str(type(warn)).split('<type ')[1].replace(">", '') if sys.version_info[0] is 2 else str(type(warn)).split('<class ')[1].replace(">", '')
+				print ("The value of 'warn' must be an integer not %s" % t)
+				sys.exit()
+			except IndexError:
+				pass
+
+			info = {}
+			info = {'lib': difflib,
+					'exception': exceptWarn,
+					'error': error,
+					'warning': warnMessage}
+
+			if info not in callback and difflib != None and exceptWarn != None and error != None and warnMessage != None and response == False:
+				callback.append(info)
+				return callback
+
+			if response == True:
+				message = message + str(info['warning'])
+				return message
+
 	def urlIndex(self, limit):
 		counter = []
 		urlist = []
+
 		for l in range(limit)[::10]:
 			counter.append(l)
 			if l in counter:
-				uni_url = 'http://archivodenoticias.uni.edu.ni/Articulo/Pagina/'+str(len(counter))
+				uni_url = 'https://archivodenoticias.uni.edu.ni/Articulo/Pagina/' +str(len(counter))
 				urlist.append(uni_url)
 		return urlist
 
@@ -50,9 +111,13 @@ class uniarticles(object):
 		for i in range(0,len(uni_url)):
 			res = {}
 			value = pageDelimiter.pop(-1) if len(pageDelimiter) is not 0 else limitResidue
-			req = self.session.get(uni_url[i])
-			soup = BeautifulSoup(req.content, 'html.parser')
-			articles = soup.findAll('article')
-			res = self.retrieve_results(articles, limit=value)
-			results.extend(res)
+			requests.packages.urllib3.disable_warnings()
+			req = self.ExceptionHandler("Ha ocurrido un error: ", True, Exception, 1, self.session.get, uni_url[i], verify=True)
+			try:
+				soup = BeautifulSoup(req.content, 'html.parser')
+				articles = soup.findAll('article')
+				res = self.retrieve_results(articles, limit=value)
+				results.extend(res)
+			except:
+				print(req + " because" + self.ExceptionHandler(" ", True, Exception, 3, self.session.get, uni_url[i], verify=True).split("]")[0])
 		return results
